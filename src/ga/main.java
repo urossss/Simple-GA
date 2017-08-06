7e071ec1427edfb47048f0fd67a5bec9c5102d95
Uros <urosss007@gmail.com>
2017-08-06 21:10:29 +0200

    Completed

--- src/ga/main.java
+++ src/ga/main.java
@@ -1,12 +1,26 @@
-
 package ga;
 
 public class main {
 
     public static void main(String[] args) {
-        
-        //prvi commit
-        
-    }
 
+        // Set a candidate solution
+        FitnessCalculator.setSolution("1111000000000000000000000000000000000000000000000000000000001111");
+
+        // Create an initial population
+        Population myPop = new Population(50, true);
+
+        // Evolve our population until we reach an optimum solution
+        int generationCount = 0;
+        while (myPop.getFittest().getFitness() < FitnessCalculator.getMaxFitness()) {
+            generationCount++;
+            System.out.println("Generation: " + generationCount + " Fittest: " + myPop.getFittest().getFitness() + " Genes; " + myPop.getFittest());
+            myPop = Algorithm.evolvePopulation(myPop);
+        }
+        System.out.println("Solution found!");
+        System.out.println("Generation: " + generationCount);
+        System.out.println("Genes:");
+        System.out.println(myPop.getFittest());
+
+    }
 }
